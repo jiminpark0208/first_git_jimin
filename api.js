5d74b4bbe85a2b84992d8d977de64188fbc843f6
jimin park <jimin0208@gmail.com>
2021-12-31 18:08:17 +0900

    second commit

--- api.js
+++ api.js
@@ -2,10 +2,12 @@ const express = require('express');
 const app = express () ;
 
 const sever  = app.listen (3001, () => {
-    console.log ('start Sever : localhost :3001')
+    console.log ('start Sever : localhost :3001');
 
 });
 
 app.get('api/user/:type', async (req, res)) => {
     res.send('connect.') ;
 });
+
+내용을 추가했다.
